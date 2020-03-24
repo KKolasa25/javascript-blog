a4ab5cacd214ac01ae7c8c3fe4bbfeb5b53c3ead
Kamil Kolasa <kamilkolasa19@gmail.com>
2020-03-24 21:48:38 +0100

    Add getAttribute href

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 'use strict';
 
 function titleClickHandler(event){
+  event.preventDefault();  
   const clickedElement = this;
   console.log('Link was clicked!');
 
@@ -12,18 +13,28 @@ function titleClickHandler(event){
     activeLink.classList.remove('active');
   }
 
-  /* [IN PROGRESS] add class 'active' to the clicked link */
+  /* [DONE] add class 'active' to the clicked link */
   
   clickedElement.classList.add('active');
-  console.log('clickedElement:', clickedElement);
+ // console.log('clickedElement:', clickedElement);
 
   /* [DONE] remove class 'active' from all articles */
 
+  //const activeArticles = document.querySelectorAll('.post');
+
+  //for(let activeArticle of activeArticles){
+    //activeArticle.classList.remove('active');
+//}
+  
   /* get 'href' attribute from the clicked link */
 
-  /* find the correct article using the selector (value of 'href' attribute) */
+  const articleSelector = clickedElement.getAttribute('href');
+  console.log(articleSelector);
 
-  /* add class 'active' to the correct article */
+  /* [IN PROGRESS] find the correct article using the selector (value of 'href' attribute) */
+
+  /* [IN PROGRESS] add class 'active' to the correct article */ 
+
 }
 
 const links = document.querySelectorAll('.titles a');
