867a0e8698a5594331656408250bf7f3c2820f5d
Kamil Kolasa <kamilkolasa19@gmail.com>
2020-03-24 19:15:13 +0100

    Add remove .post

--- js/script.js
+++ js/script.js
@@ -1,24 +1,35 @@
 'use strict';
 
-function titleClickHandler(event){
-  console.log('Link was clicked!');
-  console.log(event);
+function titleClickHandler(event) {
+    console.log('Link was clicked!');
+    console.log(event);
 
-  /* remove class 'active' from all article links  */
+    /* remove class 'active' from all article links  */
+    const activeLinks = document.querySelectorAll('.titles a.active');
 
-  /* add class 'active' to the clicked link */
+    for (let activeLink of activeLinks) {
+        activeLink.classList.remove('active');
+    }
 
-  /* remove class 'active' from all articles */
+    /* add class 'active' to the clicked link */
 
-  /* get 'href' attribute from the clicked link */
+    /* remove class 'active' from all articles */
 
-  /* find the correct article using the selector (value of 'href' attribute) */
+    const activeArticles = document.querySelectorAll('.post');
 
-  /* add class 'active' to the correct article */
+    for (let activeArticle of activeArticles) {
+        activeArticle.classList.remove('active');
+    }
+
+    /* get 'href' attribute from the clicked link */
+
+    /* find the correct article using the selector (value of 'href' attribute) */
+
+    /* add class 'active' to the correct article */
 }
 
 const links = document.querySelectorAll('.titles a');
 
-for(let link of links){
-  link.addEventListener('click', titleClickHandler);
+for (let link of links) {
+    link.addEventListener('click', titleClickHandler);
 }
