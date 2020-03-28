968588e75083a41235da1b6694c097d423531db7
Kamil Kolasa <kamilkolasa19@gmail.com>
2020-03-28 21:44:19 +0100

    change in eslintrc.js, remove eslintrc.yml, change in package.json

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,23 +4,18 @@ module.exports = {
         "commonjs": true,
         "es6": true
     },
-    "extends": "plugin:react/recommended",
+    "extends": "eslint:recommended",
     "globals": {
         "Atomics": "readonly",
         "SharedArrayBuffer": "readonly"
     },
     "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
         "ecmaVersion": 2018
     },
-    "plugins": [
-        "react"
-    ],
+
     "rules": {
         "semi": ["error", "always"],
-        "quotes": ["error", "double"]
+        "quotes": ["error", "single"]
     }
 
 };
